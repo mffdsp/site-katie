2fd8dbb60b56c9a092a0bfb1837799b89622e668
Mateus Felismino <45128866+mffdsp@users.noreply.github.com>
2020-01-03 02:06:05 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+
+var welcomeText = "OPA line adkokspdakop";
+
+var textContainer = document.querySelector(".welcome-text");
+var textContent = document.createTextNode(welcomeText);
+textContainer.appendChild(textContent);
